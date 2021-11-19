8b91383149a5289e48e92e667193341a152c2b61
Annsatsu <asanchezr11@ucentral.edu.co>
2021-11-19 13:08:43 -0500

    Gestion de domicilios

---
+++ src/main/java/co/edu/ucentral/ingsf/springprime/bean/IndexBean.java
@@ -0,0 +1,138 @@
+package co.edu.ucentral.ingsf.springprime.bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import co.edu.ucentral.ingsf.springprime.servicios.DataService;
+import lombok.Getter;
+import lombok.Setter;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.MenuModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@Component
+@ManagedBean
+@ViewScoped
+@Getter
+@Setter
+public class IndexBean implements Serializable {
+    private String cadena;
+    private Integer conteo;
+
+    @Autowired
+    private DataService dataService;
+    private List<String> images;
+    private MenuModel model;
+
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        model = new DefaultMenuModel();
+        for (int i = 1; i <= 4; i++) {
+            images.add("informe"+i+".jpg");
+        }
+
+        //First submenu
+        DefaultSubMenu firstSubmenu = DefaultSubMenu.builder()
+                .label("Dynamic Submenu")
+                .build();
+
+        DefaultMenuItem item = DefaultMenuItem.builder()
+                .value("External")
+                .url("http://www.primefaces.org")
+                .icon("pi pi-home")
+                .build();
+        firstSubmenu.getElements().add(item);
+
+        model.getElements().add(firstSubmenu);
+
+        //Second submenu
+        DefaultSubMenu secondSubmenu = DefaultSubMenu.builder()
+                .label("Dynamic Actions")
+                .build();
+
+        item = DefaultMenuItem.builder()
+                .value("Save")
+                .icon("pi pi-save")
+                .command("#{menuView.save}")
+                .update("messages")
+                .build();
+        secondSubmenu.getElements().add(item);
+
+        item = DefaultMenuItem.builder()
+                .value("Delete")
+                .icon("pi pi-times")
+                .command("#{menuView.delete}")
+                .ajax(false)
+                .build();
+        secondSubmenu.getElements().add(item);
+
+        item = DefaultMenuItem.builder()
+                .value("Redirect")
+                .icon("pi pi-search")
+                .command("#{menuView.redirect}")
+                .build();
+        secondSubmenu.getElements().add(item);
+
+        model.getElements().add(secondSubmenu);
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public MenuModel getModel() {
+        return model;
+    }
+
+    public void save() {
+        addMessage("Success", "Data saved");
+    }
+
+    public void update() {
+        addMessage("Success", "Data updated");
+    }
+
+    public void delete() {
+        addMessage("Success", "Data deleted");
+    }
+
+    public void addMessage(String summary, String detail) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public void validar(){
+        System.out.println("xxxxx   xxxx");
+
+        if (cadena.isEmpty()){
+            conteo = 0;
+        }else{
+            conteo = cadena.length();
+        }
+
+    }
+    public String ir(){
+        System.out.println("- - - - - - CLICK "+cadena);
+        if (cadena.equals("IR"))
+            return "principal";
+        else
+            return "";
+    }
+    public String guardar(){
+        dataService.crear("metodo");
+        return "";
+
+    }
+
+}
