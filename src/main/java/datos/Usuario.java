bd041f11b3574a70fd1c8f97d9f41786c6a6df21
Annsatsu <asanchezr11@ucentral.edu.co>
2021-11-19 13:29:54 -0500

    Gestion de domicilios

---
+++ src/main/java/datos/Usuario.java
@@ -0,0 +1,44 @@
+package datos;
+
+public class Usuario {
+    private String nombreUsuario;
+    private String contraseña;
+    private String tipoUsuario;
+    private String correo;
+
+    public Usuario(){
+
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
