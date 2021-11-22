61e9a3c6babda80a16652dad81995245725c733a
Annsatsu <asanchezr11@ucentral.edu.co>
2021-11-22 10:05:09 -0500

    Gestion de domicilios

---
+++ src/main/java/co/edu/ucentral/ingsf/springprime/bean/CrearUsuarioBean.java
@@ -0,0 +1,23 @@
+package co.edu.ucentral.ingsf.springprime.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+
+@Component
+@ManagedBean
+@ViewScoped
+@Getter
+@Setter
+@RequestScoped
+
+public class CrearUsuarioBean {
+    public void crearUsuario(){
+
+    }
+
+}
